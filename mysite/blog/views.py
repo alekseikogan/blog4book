4f30b703273ebe1d6566ae1c6e8de895beeba902
Alexey Kogan <alekseikogan@yandex.ru>
2024-10-18 22:39:49 +0300

    добавил CBV для списка постов

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,17 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
 
 from .models import Post
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'  # результат запроса в бд, по умочанию назвается object_list
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+    # отдает объект страницы с именем page_obj
 
 
 def post_list(request):
