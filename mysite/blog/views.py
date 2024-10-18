ff7e18611fc17cd392bcfcd03ef4e5ec39055d16
Alexey Kogan <alekseikogan@yandex.ru>
2024-10-18 22:26:13 +0300

    обработка ошибок пагинации

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 
 from .models import Post
@@ -10,7 +10,13 @@ def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
-    posts = paginator.get_page(page_number)
+
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
 
     return render(request,
                   'blog/post/list.html',
