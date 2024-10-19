eb600a383593db60e0b211cb95cea9def49871d3
Alexey Kogan <alekseikogan@yandex.ru>
2024-10-19 17:21:02 +0300

    добавил карту сайта

--- mysite/blog/sitemaps.py
+++ mysite/blog/sitemaps.py
@@ -2,3 +2,19 @@ from django.contrib.sitemaps import Sitemap
 
 from .models import Post
 
+
+class PostSitemap(Sitemap):
+    changefreq = 'weekly'
+    priority = 0.9
+
+    def items(self):
+        """QuerySet объектов, подлежащих включению в карту сайта."""
+
+        return Post.published.all()
+    
+    def lastmod(self, obj):
+        """Получает каждый возвращаемый методом items() объект 
+        и возвращает время последнего изменения объекта."""
+
+        return obj.updated
+    
