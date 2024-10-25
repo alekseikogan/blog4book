ae4d75cd538988c32182faf212741a3b59a53660
Alexey Kogan <alekseikogan@yandex.ru>
2024-10-25 09:45:24 +0400

    добавил вес слов

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,5 @@
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import (SearchQuery, SearchRank,
+                                            SearchVector)
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models import Count
@@ -143,9 +144,19 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector(
+                                'title',
+                                weigth='A', 
+                                config='russian') + \
+                            SearchVector(
+                                'body',
+                                weigth='B',
+                                config='russian')
+            search_query = SearchQuery(query, config='russian')
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
         
     return render(request,
                   'blog/post/search.html',
